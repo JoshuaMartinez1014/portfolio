d4386117c049f4c12d863e48800f5aa221b15ba1
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-29 02:48:37 -0500

    created Header component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section
+      style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}
+    >
+      <div style={{ width: "80vw" }}>
+        <img
+          src="https://cdn.pixabay.com/photo/2016/11/22/23/13/dog-1851107_1280.jpg"
+          style={{ height: "500px" }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../styles/global.css";
+
+import linkedIn from "../img/linkedin.png";
+import github from "../img/github-sign.png";
+
+const Header = () => {
+  return (
+    <div
+      className="header-container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "30px",
+      }}
+    >
+      <div className="header-section">
+        <h2>
+          Joshua Martinez &nbsp;&nbsp;
+          <span style={{ color: "grey" }}>- Portfolio</span>
+        </h2>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/
+joshua-martinez1014
+"
+            target="_blank"
+          >
+            <img
+              className="header-icon"
+              src={linkedIn}
+              style={{
+                height: "25px",
+                marginRight: "20px",
+                filter: "grayscale(75%)",
+              }}
+            />
+          </a>
+          <a href="https://github.com/JoshuaMartinez1014" target="_blank">
+            <img
+              className="header-icon"
+              src={github}
+              style={{
+                height: "27px",
+                marginRight: "20px",
+              }}
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
